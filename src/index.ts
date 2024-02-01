fe6ad5ec283de058699868a58df397c87cc82145
Vladyslav Skrynnyk <feelthatvib3@gmail.com>
2024-02-01 21:40:23 +0100

    Initialize the bot

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import { CommandKit } from 'commandkit';
+import path from 'path';
+import 'dotenv/config';
+
+const { TOKEN } = process.env;
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
+
+new CommandKit({
+	client,
+	commandsPath: path.join(__dirname, 'commands'),
+	eventsPath: path.join(__dirname, 'events'),
+	devGuildIds: ['1202649097698287667'],
+	devUserIds: ['1028751824670433364'],
+	skipBuiltInValidations: false,
+	bulkRegister: true,
+});
+
+client.login(TOKEN);
